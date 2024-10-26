55979b9d41dc185a88662fc2a48d67d4bd09578b
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-26 18:00:35 +0900

    feat: setup tailwind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return <div className="text-blue-500">Hello World</div>;
 }
 
 export default App;
