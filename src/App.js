6ae7d47b08b9c73539ac940fd344cc4b3efb32fa
bruno maruya <brun0yuj1ym02@gmail.com>
2024-11-05 15:04:55 +0900

    feat: add Logo and change bg to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterProfile from "./pages/CharacterProfile";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
     <div>
+      <div className="bg-[url('./assets/images/genshin.jpeg')] bg-cover bg-center fixed h-screen w-full top-0 left-0 z-[-3]"></div>
+      <div className="fixed inset-0 bg-black opacity-50 z-[-2]"></div>
       <Router>
+        <Link to="/">
+          <img src={logo} className="inline h-6 m-6 " />
+        </Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/characters/:id" element={<CharacterProfile />} />
